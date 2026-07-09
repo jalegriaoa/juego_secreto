81e2d1dca2da15d627e8f1db02c4f6a6e1d47d15
jalegriaoa <jalegriaoa@gmail.com>
2026-07-08 20:46:01 -0400

    Ajuste del valor máximo

---
+++ app.js
@@ -0,0 +1,26 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados =[];
+let numeroMaximo = 100;
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el numero en ${intentos}`);
+        document.getElementById('reiniciar').removeAttribute;
+
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        intentos++;
+    }
+}
